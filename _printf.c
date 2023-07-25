bd62209a85e5a8f4d2d20282dc5bbc2bdc7f2228
Medidali <m.mohamedidali.com>
2023-07-25 10:10:32 -0700

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -5,38 +5,43 @@
 *
 *Return: return a int
 */
-
 int _printf(const char *format, ...)
 {
 
-int count = 0;
-int i, j;
+int i, j, count = 0;
 
 va_list args;
 va_start(args, format);
 
 for (i = 0; format[i] != '\0'; i++)
 {
+
 if (format[i] == '%')
 {
 i++;
+
 if (format[i] == 'c')
 {
+
 char c = va_arg(args, int);
 write(1, &c, 1);
 count++;
 }
 else if (format[i] == 's')
 {
+
 char *s = va_arg(args, char*);
 for (j = 0; s[j] != '\0'; j++)
 {
+
 write(1, &s[j], 1);
 count++;
 }
 }
+
 else if (format[i] == '%')
 {
+
 write(1, "%", 1);
 count++;
 }
@@ -46,10 +51,12 @@ else
 }
 else
 {
+
 write(1, &format[i], 1);
 count++;
 }
 }
+
 va_end(args);
 return (count);
 }
